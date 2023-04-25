c8a1e7470da59de060cd374e2f7c2d1c2c1b0df7
Renato M. Costa <127049792+rnatocosta@users.noreply.github.com>
2023-04-24 22:27:52 -0300

    Create Ex3.cs

---
+++ Ex3.cs
@@ -0,0 +1,9 @@
+//  3=>Crie um programa que exiba uma sequencia de valores booleanos
+
+Boolean[] b = new Boolean[] { true, false, true, false, true };
+
+Console.WriteLine("Sequência de Booleanos: ");
+for (int ib = 0; ib < b.Length; ib++)
+{
+    Console.Write(b[ib] + " ");
+}
