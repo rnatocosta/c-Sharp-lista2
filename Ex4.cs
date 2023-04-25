62e0934ba1d3cbe3f6b5b54627d93743381d0536
Renato M. Costa <127049792+rnatocosta@users.noreply.github.com>
2023-04-24 22:29:27 -0300

    Create Ex4.cs

---
+++ Ex4.cs
@@ -0,0 +1,11 @@
+//   4=>Crie um programa que exiba uma sequencia de valores doubles
+
+Double[] d = new Double[] { 1111.11d, 22222.22d, 33333.33d, 444444.44d, 5555555.55d };
+
+Console.WriteLine("Sequência de Doubles: ");
+
+for (int i = 0; i < d.Length; i++)
+{
+
+    Console.Write(d[i] + " ");
+}
