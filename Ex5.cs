4665ed3d67c0b19fbabdd38760aca2b82792b428
Renato M. Costa <127049792+rnatocosta@users.noreply.github.com>
2023-04-24 22:30:39 -0300

    Create Ex5.cs

---
+++ Ex5.cs
@@ -0,0 +1,10 @@
+// 5=>Crie um programa que exiba uma sequencia de valores decimais
+
+Decimal[] de = new Decimal[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+Console.WriteLine("Sequência de Doubles: ");
+for (int i = 0; i < de.Length; i++)
+{
+
+    Console.Write(de[i] + " ");
+}
