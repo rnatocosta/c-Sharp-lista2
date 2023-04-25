dacc45235e1619fe3a48bdb24ce5c40bd489eafa
Renato M. Costa <127049792+rnatocosta@users.noreply.github.com>
2023-04-24 22:23:42 -0300

    Create Ex2.cs

---
+++ Ex2.cs
@@ -0,0 +1,10 @@
+// 2=>Crie um programa que exiba uma senquencia de frases
+
+string[] str = new string[] { "a frases", "b frases", "c frases", "d frases", "e frases", "f frases", "g frases", "h frases" };
+Console.WriteLine("Sequência de frases: ");
+
+for (int io = 0; io < str.Length; io++)
+{
+
+    Console.Write(str[io] + " ");
+}
