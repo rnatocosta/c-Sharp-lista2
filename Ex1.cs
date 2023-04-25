35ff57f5337d5159d415a99c13a3b399f5beb499
Renato M. Costa <127049792+rnatocosta@users.noreply.github.com>
2023-04-24 22:19:54 -0300

    Create Ex1.cs

---
+++ Ex1.cs
@@ -0,0 +1,8 @@
+//   1=>Crie um programa que exiba uma sequencia de numeros inteiros
+
+Console.WriteLine("Sequencia de numeros inteiros");
+
+for (int ii = 0; ii < 10; ii++)
+{
+    Console.Write(" " + ii);
+}
